98a3a4ca7a2e4c524930b6ac7fac44b28b68a792
Lavanya-4321 <116573812+Lavanya-4321@users.noreply.github.com>
2025-10-27 12:40:16 +0530

    Update Sidebar.jsx

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
 
   const getAllThreads = async () => {
     try {
-      const response = await fetch("http://localhost:8080/api/thread");
+      const response = await fetch("https://chatclone-0k0t.onrender.com/api/thread");
       const res = await response.json();
       const filteredData = res.map(thread => ({ threadId: thread.threadId, title: thread.title }));
       setAllThreads(filteredData);
@@ -37,7 +37,7 @@ function Sidebar() {
   const changeThread = async (newThreadId) => {
     setCurrThreadId(newThreadId);
     try {
-      const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+      const response = await fetch(`https://chatclone-0k0t.onrender.com/api/thread/${newThreadId}`);
       const res = await response.json();
       setPrevChats(res);
       setNewChat(false);
@@ -52,7 +52,7 @@ function Sidebar() {
 
   const deleteThread = async (threadId) => {
     try {
-      await fetch(`http://localhost:8080/api/thread/${threadId}`, { method: "DELETE" });
+      await fetch(`https://chatclone-0k0t.onrender.com/api/thread/${threadId}`, { method: "DELETE" });
       setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
 
       if (threadId === currThreadId) {
